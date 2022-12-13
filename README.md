3eaf876f44f1752576507fcfdc4bf5c0d20e8814
Sergey Tihon <sergey_tihon@epam.com>
2022-12-13 15:01:11 +0100

    feat: ws initi

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # streamdeck-homebridge-rs
 Experimental PoC of rewriting https://github.com/sergey-tihon/streamdeck-homebridge in Rust
+
+## Credits
+
+* [mdonoughe/streamdeck-rs](https://github.com/mdonoughe/streamdeck-rs/blob/main/src/registration.rs)
+* [mdonoughe/sbzdeck](https://github.com/mdonoughe/sbzdeck)
+* [iluvcapra/reaper_stream_deck](https://github.com/iluvcapra/reaper_stream_deck)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use futures::StreamExt;
+use serde::{Deserialize, Serialize};
+use streamdeck_rs::{registration::RegistrationParams, StreamDeckSocket};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Empty();
+
+#[tokio::main(worker_threads = 1)]
+async fn main() {
+    let params = &RegistrationParams::from_args(env::args()).unwrap();
+
+    let socket = StreamDeckSocket::<
+        Empty, //MyGlobalSettings,
+        Empty, //MyActionSettings,
+        Empty, //FromInspector,
+        Empty, //ToInspector,
+    >::connect(
+        params.port,
+        params.event.to_string(),
+        params.uuid.to_string(),
+    )
+    .await
+    .expect("connection failed");
+
+    let (mut _sink, mut _stream) = socket.split();
 }
